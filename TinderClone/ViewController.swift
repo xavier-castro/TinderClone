bb5c14adc5d0984d22e96a379dc453f610560062
xavier-castro <xavier_castro@icloud.com>
2019-03-29 16:07:07 -0500

    Add assets

--- TinderClone/ViewController.swift
+++ TinderClone/ViewController.swift
@@ -12,7 +12,30 @@ class ViewController: UIViewController {
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+
+		let subviews = [UIColor.gray, UIColor.darkGray, UIColor.black].map { (color) -> UIView in
+			let v = UIView()
+			v.backgroundColor = color
+			return v
+		}
+
+		let topStackView = UIStackView(arrangedSubviews: subviews)
+		topStackView.distribution = .fillEqually
+		topStackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+
+		let blueView = UIView()
+		blueView.backgroundColor = .blue
+
+		let yellowView = UIView()
+		yellowView.backgroundColor = .yellow
+		yellowView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+
+		let stackView = UIStackView(arrangedSubviews: [topStackView, blueView, yellowView])
+//		stackView.distribution = .fillEqually
+		stackView.axis = .vertical
+
+		view.addSubview(stackView)
+		stackView.fillSuperview()
 	}
 
 
