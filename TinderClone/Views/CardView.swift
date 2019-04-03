a5ddf72691d8d477e8a188fbc2b821c171c1b5d3
xavier-castro <xavier_castro@icloud.com>
2019-04-03 18:02:24 -0500

    Add Multiple tappable images

--- TinderClone/Views/CardView.swift
+++ TinderClone/Views/CardView.swift
@@ -12,9 +12,18 @@ class CardView: UIView {
 
 	var cardViewModel: CardViewModel! {
 		didSet {
-			imageView.image = UIImage(named: cardViewModel.imageName)
+			let imageName = cardViewModel.imageNames.first ?? ""
+			imageView.image = UIImage(named: imageName)
 			informationLabel.attributedText = cardViewModel.attributedString
 			informationLabel.textAlignment = cardViewModel.textAlignment
+
+			// Some dummy bars for now
+			(0..<cardViewModel.imageNames.count).forEach { (_) in
+				let barView = UIView()
+				barView.backgroundColor = barDeselectedColor
+				barsStackView.addArrangedSubview(barView)
+			}
+			barsStackView.arrangedSubviews.first?.backgroundColor = .white
 		}
 	}
 
@@ -31,32 +40,71 @@ class CardView: UIView {
 
 		let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
 		addGestureRecognizer(panGesture)
+		addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
 	}
+
+	var imageIndex = 0
+	fileprivate let barDeselectedColor = UIColor(white: 0, alpha: 0.1)
+
+	@objc fileprivate func handleTap(gesture: UITapGestureRecognizer) {
+		let tapLocation = gesture.location(in: nil)
+		let shouldAdvanceNextPhoto = tapLocation.x > frame.width / 2 ? true : false
+		if shouldAdvanceNextPhoto {
+			imageIndex = min(imageIndex + 1, cardViewModel.imageNames.count - 1)
+		} else {
+			imageIndex = max(0, imageIndex - 1)
+		}
+
+		let imageName = cardViewModel.imageNames[imageIndex]
+		imageView.image = UIImage(named: imageName)
+		barsStackView.arrangedSubviews.forEach { (v) in
+			v.backgroundColor = barDeselectedColor
+		}
+		barsStackView.arrangedSubviews[imageIndex].backgroundColor = .white
+
+	}
+
 	fileprivate func setupLayout() {
 		// Custom drawing code
 		layer.cornerRadius = 10
 		clipsToBounds = true
 
+		setupBarsStackView()
+
 		imageView.contentMode = .scaleAspectFill
 		addSubview(imageView)
 		imageView.fillSuperview()
+
+		setupBarsStackView()
 
 		// Add a gradient layout
 		setupGradientLayer()
 
 		addSubview(informationLabel)
-		informationLabel.anchor(top: nil, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 0, left: 16, bottom: 16, right: 16))
+		informationLabel.anchor(top: nil, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor,
+			padding: .init(top: 0, left: 16, bottom: 16, right: 16))
 
 		informationLabel.textColor = .white
 		informationLabel.numberOfLines = 0
+	}
+
+	fileprivate let barsStackView = UIStackView()
+
+	fileprivate func setupBarsStackView() {
+		addSubview(barsStackView)
+		barsStackView.anchor(top: topAnchor, leading: leadingAnchor, bottom: nil, trailing: trailingAnchor,
+			padding: .init(top: 8, left: 8, bottom: 0, right: 8), size: .init(width: 0, height: 4))
+
+		barsStackView.spacing = 4
+		barsStackView.distribution = .fillEqually
 	}
 
 	fileprivate func setupGradientLayer() {
 		// How we can draw a gradient with Swift
 		gradientLayer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
 		gradientLayer.locations = [0.5, 1.1]
-
 		layer.addSublayer(gradientLayer)
+
 	}
 
 	override func layoutSubviews() {
