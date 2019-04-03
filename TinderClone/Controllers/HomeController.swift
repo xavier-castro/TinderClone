237e41584b9b9c4b53567ecab512557bc990e597
xavier-castro <xavier_castro@icloud.com>
2019-04-02 22:13:12 -0500

    Refactored code

--- TinderClone/Controllers/HomeController.swift
+++ TinderClone/Controllers/HomeController.swift
@@ -14,10 +14,16 @@ class HomeController: UIViewController {
 	let cardsDeckView = UIView()
 	let buttonsStackView = HomeBottomControlsStackView()
 
-	let cardViewModels = [
-		User(name: "Kelly", age: 23, profession: "DJ", imageName: "lady5c").toCardViewModel(),
-		User(name: "Jane", age: 18, profession: "Teacher", imageName: "lady4c").toCardViewModel()
-	]
+	let cardViewModels: [CardViewModel] = {
+		let producers = [
+			User(name: "Kelly", age: 23, profession: "DJ", imageName: "lady5c"),
+			User(name: "Jane", age: 18, profession: "Teacher", imageName: "lady4c"),
+			Advertiser(title: "Slide Out Menu", brandName: "Lets Build That App", posterPhotoName: "slide_out_menu_poster")
+		] as [ProducesCardViewModel]
+
+		let viewModels = producers.map({return $0.toCardViewModel()})
+		return viewModels
+	}()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -30,9 +36,7 @@ class HomeController: UIViewController {
 	fileprivate func setupDummyCards() {
 		cardViewModels.forEach { (cardVM) in
 			let cardView = CardView(frame: .zero)
-			cardView.imageView.image = UIImage(named: cardVM.imageName)
-			cardView.informationLabel.attributedText = cardVM.attributedString
-			cardView.informationLabel.textAlignment = cardVM.textAlignment
+			cardView.cardViewModel = cardVM
 			cardsDeckView.addSubview(cardView)
 			cardView.fillSuperview()
 		}
---
+++ TinderClone/Models/Advertiser.swift
@@ -0,0 +1,22 @@
+//
+//  Advertiser.swift
+//  TinderClone
+//
+//  Created by Xavier Castro on 4/2/19.
+//  Copyright © 2019 Xavier Castro. All rights reserved.
+//
+
+import UIKit
+
+struct Advertiser: ProducesCardViewModel {
+	let title: String
+	let brandName: String
+	let posterPhotoName: String
+
+	func toCardViewModel() -> CardViewModel {
+		let attributedString = NSMutableAttributedString(string: title, attributes: [.font: UIFont.systemFont(ofSize: 34, weight: .heavy)])
+		attributedString.append(NSAttributedString(string: "\n" + brandName, attributes: [.font: UIFont.systemFont(ofSize: 24, weight: .bold)]))
+
+		return CardViewModel(imageName: posterPhotoName, attributedString: attributedString, textAlignment: .center)
+	}
+}
