7468e3df2d369252c5f1eea875cf70a84941939f
xavier-castro <xavier_castro@icloud.com>
2019-04-02 19:44:44 -0500

    Add card dismissal

--- TinderClone/Views/CardView.swift
+++ TinderClone/Views/CardView.swift
@@ -12,6 +12,9 @@ class CardView: UIView {
 
 	fileprivate let imageView = UIImageView(image: #imageLiteral(resourceName: "lady5c"))
 
+	// Configurations
+	fileprivate let threshold: CGFloat = 100
+
 	override init(frame: CGRect) {
 		super.init(frame: frame)
 		layer.cornerRadius = 10
@@ -29,7 +32,7 @@ class CardView: UIView {
 		case .changed:
 			handleChanged(gesture)
 		case .ended:
-			handleEnded()
+			handleEnded(gesture)
 		default:
 			()
 		}
@@ -38,18 +41,24 @@ class CardView: UIView {
 
 	fileprivate func handleChanged(_ gesture: UIPanGestureRecognizer) {
 		let translation = gesture.translation(in: nil)
-		// Rotation
 		let degrees: CGFloat = translation.x / 20
 		let angle = degrees * .pi / 180
 		let rotationalTransformation = CGAffineTransform(rotationAngle: angle)
 		self.transform = rotationalTransformation.translatedBy(x: translation.x, y: translation.y)
 	}
 
-	fileprivate func handleEnded() {
-		UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
-			self.transform = .identity
-		}) { (_) in
+	fileprivate func handleEnded(_ gesture: UIPanGestureRecognizer) {
+		let shouldDismissCard = gesture.translation(in: nil).x > threshold
 
+		UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
+			if shouldDismissCard {
+				self.frame = CGRect(x: 1000, y: 0, width: self.frame.width, height: self.frame.height)
+			} else {
+				self.transform = .identity
+			}
+		}) { (_) in
+			self.transform = .identity
+			self.frame = CGRect(x: 0, y: 0, width: self.superview!.frame.width, height: self.superview!.frame.height)
 		}
 	}
 
